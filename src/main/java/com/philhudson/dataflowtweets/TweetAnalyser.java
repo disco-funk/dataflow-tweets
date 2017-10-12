04b3e61020358aff0d9d92a74bc1f3a289ae8648
Hudson, Philip <Philip.Hudson@zuhlke.com>
2017-10-12 12:46:55 +0200

    (refactor): use google cloud sdk 2.1.0

--- src/main/java/com/philhudson/dataflowtweets/TweetAnalyser.java
+++ src/main/java/com/philhudson/dataflowtweets/TweetAnalyser.java
@@ -3,15 +3,15 @@ package com.philhudson.dataflowtweets;
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
-import com.google.cloud.dataflow.sdk.Pipeline;
-import com.google.cloud.dataflow.sdk.io.BigQueryIO;
-import com.google.cloud.dataflow.sdk.options.BigQueryOptions;
-import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
-import com.google.cloud.dataflow.sdk.runners.BlockingDataflowPipelineRunner;
-import com.google.cloud.dataflow.sdk.transforms.DoFn;
-import com.google.cloud.dataflow.sdk.transforms.ParDo;
 import com.zuhlke.ta.prototype.SentimentAnalyzer;
 import com.zuhlke.ta.sentiment.SentimentAnalyzerImpl;
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.values.TypeDescriptor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +27,7 @@ public class TweetAnalyser {
         BigQueryOptions options = PipelineOptionsFactory
                 .create()
                 .as(BigQueryOptions.class);
-        options.setRunner(BlockingDataflowPipelineRunner.class);
+        options.setRunner(DataflowRunner.class);
         options.setProject(PROJECT_ID);
         options.setTempLocation(STAGING_LOCATION);
 
@@ -39,23 +39,22 @@ public class TweetAnalyser {
 
         Pipeline pipeline = Pipeline.create(options);
 
-        pipeline.apply(BigQueryIO.Read
-                .named("Load Tweet Input")
+        pipeline.apply(BigQueryIO.read()
                 .from("genuine-axe-182507:intalert.test"))
-                .apply(ParDo.named("Analyse Sentiment").of(new DoFn<TableRow, TableRow>() {
-                    @Override
-                    public void processElement(ProcessContext c) {
-                        TableRow newRow = new TableRow();
-                        String content = (String) c.element().get("content");
-                        newRow.set("content", content);
-                        newRow.set("timestamp", c.element().get("timestamp"));
-                        double sentiment = analyzer.getSentiment(content);
-                        newRow.set("sentiment", sentiment);
-                        c.output(newRow);
-                    }
-                }))
-                .apply(BigQueryIO.Write
-                        .named("Write Output Analysed Tweets")
+                .apply(
+                    MapElements
+                        .into(TypeDescriptor.of(TableRow.class))
+                        .via((TableRow c) -> {
+                            TableRow newRow = new TableRow();
+                            String content = (String) c.get("content");
+                            newRow.set("content", content);
+                            newRow.set("timestamp", c.get("timestamp"));
+                            double sentiment = analyzer.getSentiment(content);
+                            newRow.set("sentiment", sentiment);
+                            return newRow;
+                        })
+                )
+                .apply(BigQueryIO.writeTableRows()
                         .to("genuine-axe-182507:intalert.analysed")
                         .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
                         .withSchema(schema));
